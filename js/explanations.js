54d61ef7dab059cfb50b264bbbf425d8609444c8
Amany Horani <1221305230@student.mmu.edu.my>
2024-07-21 04:53:38 -0700

    Made some car stuff happen!

---
+++ js/explanations.js
@@ -0,0 +1,31 @@
+//  Explanations
+
+// Variables
+var favColor = 'blue';
+var myFavColors = ['blue', 'red', 'green'];
+var numOfFavColors = 3;
+var hasGotFavColors = true;
+var richObject = {
+	firstName: 'Rich',
+	lastName: 'Armstrong',
+	favColors: ['blue', 'red', 'green'],
+	yearsAlive: 103,
+	isMale: true
+};
+
+// Functions
+var whatIsMyFavColor = function () {
+	return 'blue';
+}
+
+var doSomething = function () {
+	console.log("do Something!!!");
+}
+
+var area = function (width, height) {
+	return width * height;
+}
+
+var fullname = function (firstname, secondname) {
+	return firstname + ' ' + secondname;
+}
